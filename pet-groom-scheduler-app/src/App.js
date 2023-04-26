befecf53b2ad99ae5eec8f2c6f30b7351cd79f86
Sukanya <sukanyajain1@gmail.com>
2023-04-25 20:17:28 -0400

    first commit project

---
+++ pet-groom-scheduler-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App container">
+      <h1>Dojo Pet Grooming - We take care of your pet while you work!</h1>
+      <Form firstName = "" lastName = "" email = "" password = "" confirmPw = "">
+
+      </Form>
+    </div>
+  );
+}
+
+export default App;
